d4ebeb67a36ae8c511f9261a98b3b8dec87a0d71
Johnny Wevany <johnny.wevany@moredev.tech>
2023-07-12 18:22:46 -0300

    refactor: change imports to use module

--- src/domain/test/mock.authentication.ts
+++ src/domain/test/mock.authentication.ts
@@ -1,4 +1,4 @@
-import { AuthenticationParams } from '../../domain/useCases/authentication'
+import { AuthenticationParams } from '@/domain/useCases/authentication'
 import { faker } from '@faker-js/faker'
 
 export const mockAuthentication = (): AuthenticationParams => ({
