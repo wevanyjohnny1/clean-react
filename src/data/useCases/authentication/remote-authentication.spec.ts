15ba6b2cc92ade1e6a00835ff230d06e535da8f5
Johnny Wevany <johnny.wevany@moredev.tech>
2023-07-12 19:02:31 -0300

    chore: add

--- src/data/useCases/authentication/remote-authentication.spec.ts
+++ src/data/useCases/authentication/remote-authentication.spec.ts
@@ -1,4 +1,4 @@
-import { mockAuthentication } from '@/domain/test/mock.authentication'
+import { mockAccountModel, mockAuthentication } from '@/domain/test/mock.account'
 import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
 
@@ -72,5 +72,16 @@ describe('RemoteAuthentication', () => {
     }
     const promise = sut.auth(mockAuthentication())
     await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  test('Should return an AccountModel if HttpPostClient returns 200', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const httpResult = mockAccountModel()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult
+    }
+    const account = await sut.auth(mockAuthentication())
+    expect(account).toEqual(httpResult)
   })
 })
--- src/data/useCases/authentication/remote-authentication.ts
+++ src/data/useCases/authentication/remote-authentication.ts
@@ -3,9 +3,9 @@ import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
 import { AccountModel } from '@/domain/models/account-model'
-import { AuthenticationParams } from '@/domain/useCases/authentication'
+import { Authentication, AuthenticationParams } from '@/domain/useCases/authentication'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
@@ -13,13 +13,13 @@ export class RemoteAuthentication {
 
   }
 
-  async auth (params: AuthenticationParams): Promise<void> {
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
     const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return httpResponse.body
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
       case HttpStatusCode.badRequest: throw new UnexpectedError()
       default: throw new UnexpectedError()
